47ab1d36f74fd859003ec0324672544b579e6f2d
setrar <setrar@valiha.com>
2019-03-12 11:33:59 -0400

    Update Tree.scala

--- src/main/g8/src/main/scala/adt/Tree.scala
+++ src/main/g8/src/main/scala/adt/Tree.scala
@@ -2,19 +2,56 @@ package adt
 
 sealed trait ???
 
-object List {
+object Tree {
 
-  val coucou = "coucou"
+  def size[A](t: Tree[A]): Int = ???
 
-  def sum(ints: List[Int]): Int = ???
+  /*
+  We're using the method `max` that exists on all `Int` values rather than an explicit `if` expression.
+  
+  Note how similar the implementation is to `size`. We'll abstract out the common pattern in a later exercise. 
+  */
+  def maximum(t: Tree[Int]): Int = ???
 
-  def product(ds: List[Double]): Double = ???
+  /*
+  Again, note how similar the implementation is to `size` and `maximum`.
+  */
+  def depth[A](t: Tree[A]): Int = ???
 
-  def apply[A](as: A*): List[A] =
-    if (as.isEmpty) {
-      Nil
-    } else {
-      Cons(as.head, apply(as.tail: _*))
-    }
+  def map[A,B](t: Tree[A])(f: A => B): Tree[B] = ???
 
+  /* 
+  Like `foldRight` for lists, `fold` receives a "handler" for each of the data constructors of the type, and recursively
+  accumulates some value using these handlers. As with `foldRight`, `fold(t)(Leaf(_))(Branch(_,_)) == t`, and we can use
+  this function to implement just about any recursive function that would otherwise be defined by pattern matching.
+  */
+  def fold[A,B](t: Tree[A])(f: A => B)(g: (B,B) => B): B = ???
+  
+  def sizeViaFold[A](t: Tree[A]): Int = ???
+  
+  def maximumViaFold(t: Tree[Int]): Int = ???
+  
+  def depthViaFold[A](t: Tree[A]): Int = ???
+  
+  /*
+  Note the type annotation required on the expression `Leaf(f(a))`. Without this annotation, we get an error like this: 
+  
+  type mismatch;
+    found   : fpinscala.datastructures.Branch[B]
+    required: fpinscala.datastructures.Leaf[B]
+       fold(t)(a => Leaf(f(a)))(Branch(_,_))
+                                      ^  
+  
+  This error is an unfortunate consequence of Scala using subtyping to encode algebraic data types. Without the
+  annotation, the result type of the fold gets inferred as `Leaf[B]` and it is then expected that the second argument
+  to `fold` will return `Leaf[B]`, which it doesn't (it returns `Branch[B]`). Really, we'd prefer Scala to
+  infer `Tree[B]` as the result type in both cases. When working with algebraic data types in Scala, it's somewhat
+  common to define helper functions that simply call the corresponding data constructors but give the less specific
+  result type:
+    
+    def leaf[A](a: A): Tree[A] = Leaf(a)
+    def branch[A](l: Tree[A], r: Tree[A]): Tree[A] = Branch(l, r)
+  */
+  def mapViaFold[A,B](t: Tree[A])(f: A => B): Tree[B] = ???
+  
 }
